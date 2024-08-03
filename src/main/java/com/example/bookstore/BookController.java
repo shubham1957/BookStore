8dc8be914fb4c5e66f6ba2101cc1c595f0118224
Shubham Sinha <sinhashubham371@gmail.com>
2024-08-03 16:22:13 +0530

    created first get api

--- src/main/java/com/example/bookstore/BookController.java
+++ src/main/java/com/example/bookstore/BookController.java
@@ -1,6 +1,7 @@
 package com.example.bookstore;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -8,11 +9,17 @@ import java.util.List;
 
 @RestController
 public class BookController {
-    public static List<Book> books;
+    public static List<Book> bookList;
     public  BookController(){
-        books = new ArrayList<>();
-        books.add(new Book(2020,"Book1",1));
-        books.add(new Book(2021,"Book2",2));
+        bookList = new ArrayList<>();
+        bookList.add(new Book(2020,"Book1",1));
+        bookList.add(new Book( 2021,"Book2",2));
+        bookList.add(new Book(2022,"Book3",3));
+    }
+
+    @GetMapping("/books")
+    public List<Book> getBooks(){
+        return bookList;
     }
 
 }
